7df78e862a06d26bb734aea11b0b9ee6cde0ccfb
mt1516 <tongman1516@gmail.com>
2024-11-30 20:35:16 +0800

    cpu for aws

--- aws_inference_service.py
+++ aws_inference_service.py
@@ -6,22 +6,17 @@ from src.net import Net
 
 app = Flask(__name__)
 
-def load_model(model_path):
+@app.route('/predict', methods=['POST'])
+def predict():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = Net()
-    model.load_state_dict(torch.load(model_path))
+    model.load_state_dict(torch.load('model/model_lr_1e-2.pth', map_location=device))
     if torch.cuda.device_count() > 1:
         model = torch.nn.DataParallel(model)
     model.to(device)
-    return model
 
-model = load_model('model/model_lr_1e-2.pth')
-
-@app.route('/predict', methods=['POST'])
-def predict():
     data = request.get_json()
     data_df = pd.read_json(data, orient='split')
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     data_tensor = torch.tensor(data_df.values, dtype=torch.long).to(device)
     dataset = TensorDataset(data_tensor)
     loader = DataLoader(dataset, batch_size=20, shuffle=False)
